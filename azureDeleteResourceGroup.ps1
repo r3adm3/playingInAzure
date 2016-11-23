12e1ec532eb7c2ad8e125d9feff6a945b354437a
r3adm3 <adrianjfarnell@gmail.com>
2016-11-23 22:33:05 +0000

    Removes resource groups

---
+++ azureDeleteResourceGroup.ps1
@@ -0,0 +1,25 @@
+﻿$now = get-date
+write-host "$(get-date) - --------------------------------------------------------------------"
+write-host "$(get-date) - Del Resource Group Script starts $now (demo purposes only)"
+write-host "$(get-date) - --------------------------------------------------------------------"
+
+$resourceGroupName = "noobExample_RG"
+
+# login
+login-azurermaccount
+
+# resource group, ties related resources together (i can delete it all with one command later for example). 
+write-host "$(get-date) - delete resource group"
+remove-azurermresourcegroup -name $resourceGroupName
+
+$now2 = get-date
+# write out how long it took. 
+write-host "$(get-date) - finished. Took $(($now2 - $now).minutes) minute(s) and $(($now2 - $now).seconds) second(s)"
+
+
+
+
+
+
+
+
